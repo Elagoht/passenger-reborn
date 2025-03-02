ee0b14f1a9686699d3e73464a957f74ca4ef5c5c
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-03-03 02:09:24 +0300

    docs: Swagger operation descriptions to passphrase controller endpoints

--- src/resources/passphrases/passphrases.controller.ts
+++ src/resources/passphrases/passphrases.controller.ts
@@ -8,7 +8,7 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { JwtGuard } from 'src/utilities/Guards/jwt.guard';
 import { PassphrasesService } from './passphrases.service';
 import RequestCreatePassphrase from './schemas/requests/create';
@@ -21,21 +21,33 @@ export class PassphrasesController {
   constructor(private readonly passphrasesService: PassphrasesService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all passphrases',
+  })
   async getPassphrase() {
     return this.passphrasesService.getPassphraseEntries();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get a passphrase by ID and its history',
+  })
   async getPassphraseById(@Param('id') id: string) {
     return this.passphrasesService.getPassphraseEntryById(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a new passphrase',
+  })
   async createPassphrase(@Body() body: RequestCreatePassphrase) {
     return this.passphrasesService.createPassphraseEntry(body);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update a passphrase by ID',
+  })
   async updatePassphrase(
     @Param('id') id: string,
     @Body() body: RequestUpdatePassphrase,
@@ -44,11 +56,17 @@ export class PassphrasesController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete a passphrase by ID',
+  })
   async deletePassphrase(@Param('id') id: string) {
     return this.passphrasesService.deletePassphraseEntry(id);
   }
 
   @Get(':id/similar')
+  @ApiOperation({
+    summary: 'Get similar passphrases by ID',
+  })
   async getSimilarPassphrases(@Param('id') id: string) {
     return this.passphrasesService.getSimilarPassphraseEntries(id);
   }
