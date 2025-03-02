c3171f817e1b79b8c6cc2372bef4b2bc3b9da2a3
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-03-03 01:15:38 +0300

    chore: Swagger API documentation for passphrase request schemas

--- src/resources/auth/schemas/requests/change.ts
+++ src/resources/auth/schemas/requests/change.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RequestChangePassphrase {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The new passphrase for the user',
+    example: 'new-passphrase',
+  })
   passphrase: string;
 }
--- src/resources/auth/schemas/requests/reset.ts
+++ src/resources/auth/schemas/requests/reset.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RequestResetPassphrase {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The recovery key for the user',
+    example: 'recovery-key',
+  })
   recoveryKey: string;
 }
