f0b7c9c44252666e423175f12800632f0eb881d1
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-03-25 20:24:37 +0300

    fix: validation decorators to RequestSetPreference for improved data integrity

--- src/resources/preferences/schemas/requests/preference.ts
+++ src/resources/preferences/schemas/requests/preference.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { IsNotEmpty, IsString } from 'class-validator';
 export class RequestSetPreference {
   @ApiProperty({
     description: 'The value of the preference',
     example: 'true',
   })
+  @IsString()
+  @IsNotEmpty()
   value: string;
 }
